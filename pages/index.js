8f95da11918b990965c46e962f7326064e6d2040
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2024-02-16 12:17:00 +0000

    adição de pasta pages

---
+++ pages/index.js
@@ -0,0 +1,6 @@
+// isso é um componente react
+function Home(){
+    return <h1>Mensagem ainda não pensada</h1>
+}
+// o default indica que será o componente padrão a ser exportado
+export default Home;
