3dbd0af0d022e3c764d956b19efebfb0f90e9d7e
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2024-02-17 00:45:40 +0000

    mensagem modificada

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 // isso é um componente react
 function Home(){
-    return <h1>Mensagem ainda não pensada</h1>
+    return <h1>Elly, Esse é apenas eu testando uma nova tecnólogia, e usando ela pra dizer que de amo :)</h1>
 }
 // o default indica que será o componente padrão a ser exportado
 export default Home;
