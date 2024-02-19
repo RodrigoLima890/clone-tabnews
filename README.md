b534a787710f8687fc042d9dd715ec002c9bdc80
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2024-02-19 01:15:49 +0000

    adição de scripts lint:check e lint:read

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Projeto para aprendizado
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,12 @@
 // isso é um componente react
-function Home(){
-    return <h2>Elly, Esse é apenas eu testando uma nova tecnólogia, e usando ela pra dizer que de amo :)</h2>
+function Home() {
+  return (
+    <h2>
+      Elly, Esse é apenas eu testando uma nova tecnólogia, e usando ela pra
+      dizer que de amo :)
+    </h2>
+  );
 }
+
 // o default indica que será o componente padrão a ser exportado
 export default Home;
- 
