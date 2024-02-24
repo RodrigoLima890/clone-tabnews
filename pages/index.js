df90d0ba33a839ac1fb06f9ff8250b1be4086844
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2024-02-24 14:43:59 +0000

    Pagina em construção

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
 // isso é um componente react
 function Home() {
-  return (
-    <h2>
-      Elly, Esse é apenas eu testando uma nova tecnólogia, e usando ela pra
-      dizer que de amo :)
-    </h2>
-  );
+  return <h2>Em Construção...</h2>;
 }
 
 // o default indica que será o componente padrão a ser exportado
