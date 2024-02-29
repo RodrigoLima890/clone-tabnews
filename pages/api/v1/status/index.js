81e4609663a23454f740e88fa6ed4861f3e4c6d3
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2024-02-29 23:46:46 +0000

    add `databas.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,5 +1,8 @@
-// /api/status
-function status(request, response) {
+import database from "../../../../infra/database.js";
+
+async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1 as sum;");
+  // console.log(result.rows);
   response.status(200).json({ chave: "valor" });
 }
 export default status;
