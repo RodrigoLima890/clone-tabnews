6c567e84ab35f023b2bb83aabf8bff9cf092dfad
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2025-02-16 23:03:11 +0000

    feat: implements errors in /status

--- infra/errors.js
+++ infra/errors.js
@@ -17,3 +17,22 @@ export class InternalSeverErro extends Error {
     };
   }
 }
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("Método não permitido para este endpoint.");
+    this.name = "MethodNotAllowedError";
+    this.action =
+      "Verifique se o método HTTP enviado é válido para este endpoint";
+    this.statusCode = 405;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,46 +1,60 @@
+import { createRouter } from "next-connect";
 import database from "infra/database.js";
-import { InternalSeverErro } from "infra/errors";
+import { InternalSeverErro, MethodNotAllowedError } from "infra/errors";
 
-async function status(request, response) {
-  try {
-    const updateAt = new Date().toISOString();
+const router = createRouter();
 
-    const maxConnectionsResult = await database.query("SHOW max_connections;");
-    const maxConnections = maxConnectionsResult.rows[0].max_connections;
-    const versionDatabaseResult = await database.query("SHOW server_version;");
-    const versionDatabase = versionDatabaseResult.rows[0].server_version;
-    const databaseName = process.env.POSTGRES_DB;
-    const openedConnectionsResult = await database.query({
-      text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname= $1;",
-      values: [databaseName],
-    });
-    const opneedConnectionsValue = openedConnectionsResult.rows[0].count;
-    const activeConnections = (
-      await database.query(
-        "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';",
-      )
-    ).rowCount;
+router.get(getHandler);
 
-    response.status(200).json({
-      update_at: updateAt,
-      dependencies: {
-        database: {
-          version: versionDatabase,
-          max_connections: parseInt(maxConnections),
-          active_connections: activeConnections,
-          opened_connections: opneedConnectionsValue,
-        },
+export default router.handler({
+  onNoMatch: onNoMatchHandler,
+  onError: onErrorHandle,
+});
+
+async function getHandler(request, response) {
+  const updateAt = new Date().toISOString();
+
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const maxConnections = maxConnectionsResult.rows[0].max_connections;
+  const versionDatabaseResult = await database.query("SHOW server_version;");
+  const versionDatabase = versionDatabaseResult.rows[0].server_version;
+  const databaseName = process.env.POSTGRES_DB;
+  const openedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname= $1;",
+    values: [databaseName],
+  });
+  const opneedConnectionsValue = openedConnectionsResult.rows[0].count;
+  const activeConnections = (
+    await database.query(
+      "SELECT COUNT(*) FROM pg_stat_activity WHERE state = 'active';",
+    )
+  ).rowCount;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: versionDatabase,
+        max_connections: parseInt(maxConnections),
+        active_connections: activeConnections,
+        opened_connections: opneedConnectionsValue,
       },
-    });
-  } catch (error) {
-    const internalSeverErro = new InternalSeverErro({
-      cause: error,
-    });
-
-    console.log("\n Erro dentro do controller");
-    console.log(internalSeverErro);
-
-    response.status(500).json(internalSeverErro);
-  }
+    },
+  });
 }
-export default status;
+
+function onNoMatchHandler(request, response) {
+  const publicErrorObject = new MethodNotAllowedError();
+  response.status(publicErrorObject.statusCode).json(publicErrorObject);
+}
+
+function onErrorHandle(error, request, response) {
+  const internalSeverErro = new InternalSeverErro({
+    cause: error,
+  });
+
+  console.log("\n Erro dentro do next-connect");
+  console.log(internalSeverErro);
+
+  response.status(500).json(internalSeverErro);
+}
