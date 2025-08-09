a54ed6ace99e92a19e4432391146d02fd6d99a07
Rodrigo Das Chagas De Lima <104842609+RodrigoLima890@users.noreply.github.com>
2025-08-09 20:35:26 +0000

    feat: migrator and tests post for users

---
+++ tests/integration/api/v1/users/post.test.js
@@ -0,0 +1,21 @@
+import orchestrator from "tests/orchestrator";
+import database from "infra/database";
+
+beforeAll(async () => {
+  await orchestrator.waitForAllServices();
+  await orchestrator.clearDatabase();
+  await orchestrator.runPendingMigrations();
+});
+
+describe("POST /api/v1/users", () => {
+  describe("Anonimous user", () => {
+    test("With unique and valid data", async () => {
+      const users = await database.query("SELECT * FROM users;");
+      console.log(users.rows);
+      const response = await fetch("http://localhost:3000/api/v1/users", {
+        method: "POST",
+      });
+      expect(response.status).toBe(201);
+    });
+  });
+});
